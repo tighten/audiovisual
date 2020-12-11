bb0145aa7ae9e452b4d927bb1f701164272dcd7e
Matt Stauffer <matt@tighten.co>
2020-12-11 09:00:25 -0500

    Add purposes

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -1,14 +1,14 @@
 <div class="max-w-3xl mx-auto sm:px-6 lg:px-8 mt-9">
-    <div class="ml-3 text-xl font-bold text-gray-400 uppercase">
+    <a href="/" class="ml-3 text-xl font-bold text-gray-400 uppercase">
         Audiovisual.dev
-    </div>
+    </a>
 </div>
 
 <div class="max-w-3xl mx-auto mt-3 sm:px-6 lg:px-8">
-    <a href="/" class="inline-block px-4 py-2 hover:underline">Home</a>
     <a href="/microphones" class="inline-block px-4 py-2 hover:underline">Microphones</a>
     <a href="/webcams" class="inline-block px-4 py-2 hover:underline">Webcams</a>
     <a href="/audio-interfaces" class="inline-block px-4 py-2 hover:underline">Audio Interfaces</a>
     <a href="/lights" class="inline-block px-4 py-2 hover:underline">Lights</a>
     <a href="/set-ups" class="inline-block px-4 py-2 hover:underline">Set-ups</a>
+    <a href="/purposes" class="inline-block px-4 py-2 hover:underline">Purposes</a>
 </div>
---
+++ resources/views/purposes.blade.php
@@ -0,0 +1,10 @@
+<x-app-layout>
+    <div class="max-w-xl mx-auto mt-12 sm:px-6 lg:px-8 page-content">
+        <h2 class="page-title">Purposes</h2>
+        <ul class="pl-6 list-disc">
+            <li><a href="/purposes/podcasting">Podcasting</a></li>
+            <li><a href="/purposes/streaming">Streaming</a></li>
+            <li><a href="/purposes/remote-working">Remote Working</a></li>
+        </ul>
+    </div>
+</x-app-layout>
