20af57f3bef182d301c3943113653f401ac9d447
Matt Stauffer <matt@tighten.co>
2021-12-08 13:05:02 -0500

    Add riverside links

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,6 +7,9 @@
             <li>https://twitter.com/EricaJoy/status/1336920432411209728</li>
             <li>https://marco.org/podcasting-microphones</li>
             <li>https://www.hanselman.com/blog/good-better-best-creating-the-ultimate-remote-worker-webcam-setup-on-a-budget</li>
+            <li>https://riverside.fm/blog/podcast-studio-setup</li>
+            <li>https://riverside.fm/blog/podcast-microphone</li>
+            <li>https://riverside.fm/blog/best-podcast-camera</li>
         </ul>
         <h2 class="mt-4 text-xl font-bold">Brainstorm features</h2>
         <ul class="pl-6 list-disc">
